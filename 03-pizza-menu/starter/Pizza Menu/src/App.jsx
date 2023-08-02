9115b9b641aeb9e41831762c174b7d28a8c062b1
genildocs <genildocs@gmail.com>
2023-08-02 16:06:22 -0300

    commit

--- 03-pizza-menu/starter/Pizza Menu/src/App.jsx
+++ 03-pizza-menu/starter/Pizza Menu/src/App.jsx
@@ -1,13 +1,67 @@
-import './App.css'
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
 
 function App() {
-
-
   return (
     <>
-        <h1>Hello Vite</h1>
+      {pizzaData.map((pizza)=>(
+        <Pizza Image={pizza.photoName} Title={pizza.name} Text={pizza.ingredients}/>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function Pizza({Title, Text, Image}) {
+  return(
+    <>
+        <img src={Image} alt="pizza" />
+        <h2>{Title}</h2>
+        <p>{Text}</p>
+    </>
+  ) 
+}
