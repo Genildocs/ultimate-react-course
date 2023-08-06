3fb8c609e9b72e82a43ad75d37bf636b857cdca1
genildocs <genildocs@gmail.com>
2023-08-06 05:55:44 -0300

    commit

--- 03-pizza-menu/starter/Pizza Menu/src/App.jsx
+++ 03-pizza-menu/starter/Pizza Menu/src/App.jsx
@@ -78,20 +78,27 @@ function Pizza({ name, ingredients, photo, price }) {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
+
+
+    {pizzas &&(
       <ul className="pizzas">
-      {pizzaData.map((pizza) => (
-        <Pizza
-          key={pizza.name}
-          photo={pizza.photoName}
-          name={pizza.name}
-          ingredients={pizza.ingredients}
-          price={pizza.price}
-        />
-      ))}
+        {pizzas.map((pizza) => (
+          <Pizza
+            key={pizza.name}
+            photo={pizza.photoName}
+            name={pizza.name}
+            ingredients={pizza.ingredients}
+            price={pizza.price}
+          />
+        ))}
       </ul>
+    )}
+      
     </main>
   );
 }
@@ -107,7 +114,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>"We're open until {closeHour}:00. Come visit us or order online"</p>
+          <button className="btn">Order</button>
+        </div>
+        
+      )}
     </footer>
   );
 }
