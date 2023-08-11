40195d98a2bfd24e2d4ef57ff301fa90e5c2dbe5
genildocs <genildocs@gmail.com>
2023-08-11 02:50:48 -0300

    commit

--- 05-travel-list/starter/travel-list/src/App.jsx
+++ 05-travel-list/starter/travel-list/src/App.jsx
@@ -1,41 +1,72 @@
-import './index.css';
+import "./index.css";
+import { useState } from "react";
 
-export default function App(){
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
       <Stats />
     </div>
-  )
+  );
 }
 
-function Logo(){
-  return <h1>🌴 Far Away 🧳</h1>
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form(){
-  return(
-    <div className='add-form'>
-      <h3>What do you need for your trip?</h3>
-    </div>
-  )
-}
+function Form() {
+  const [description, setDescription] = useState("")
 
-function PackingList(){
+  function handleSubmit(e){
+      e.preventDefault();      
+  }
+
   return (
-    <div className='list'>List</div>
-  )
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i)=> i + 1).map(num=><option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e)=>setDescription(e.target.value)}/>
+      <button>Add</button>
+    </form>
+  );
 }
 
-function Stats(){
-  return(
-    <footer className='stats'>
-      <em>
-        You have X items on your list, and you already packed X (X%)
-      </em>
-      
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
-  )
+  );
 }
